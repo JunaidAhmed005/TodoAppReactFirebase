37769a7b76acb7daedd130e72849dc8e5eab4808
JunaidAhmed <junaidahmed.jk@gmail.com>
2017-03-23 15:29:34 +0500

    Complete handleAddTodo

--- playground/spread.js
+++ playground/spread.js
@@ -3,19 +3,23 @@ function add(a,b) {
 }
 
 console.log(add(3,1));
+console.log("\n");
 
 var toAdd = [5, 9];
 // it is too difficult tto do like this
 console.log("Old index method " + add(toAdd[0], toAdd[1])); 
+console.log("\n");
 
 // instead of above method we use spread operator (...ArrayName)
 console.log("Spread operator " + add(...toAdd));
+console.log("\n");
 
 var groupA = ["Junaid", "Ahmed"];
 var groupB = ["Faraz"];
 var final = [...groupB ,3, ...groupA];
 
 console.log(final);
+console.log("\n");
 
 var person1 = ["Junaid", 23];
 var person2 = ["Daniyal", 20];
@@ -25,11 +29,14 @@ function greeting(name, age) {
 }
 
 greeting(...person1);
+console.log("\n");
+
 greeting(...person2);
+console.log("\n");
 
 var names = ["Hammad", "Faraz"];
 var final = ["Junaid", ...names];
 
 final.forEach(function(name) {
     console.log("Hi " + name);
-}, this);
+});
