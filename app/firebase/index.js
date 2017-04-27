9594f1d0f588b8256323456c8385cd03e8ebf30c
JunaidAhmed <junaidahmed.jk@gmail.com>
2017-04-28 02:06:42 +0500

    Add actions for firebase login and logout

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -16,5 +16,6 @@ try {
     console.log(e);
 }
 
+export var githubProvider = new firebase.auth.GithubAuthProvider();
 export var firebaseRef = firebase.database().ref();
 export default firebase; // root firebase llibrary 
