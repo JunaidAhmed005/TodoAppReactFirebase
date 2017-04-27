b48e70496b5ab3653d2050c0cac3075a22848c44
JunaidAhmed <junaidahmed.jk@gmail.com>
2017-04-27 22:52:02 +0500

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,17 +3,17 @@ import firebase from "firebase";
 try {
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyBufLINB25b5GDQu-NLE79gTfoGFUc-RgY",
-        authDomain: "reacttodoapp-c643b.firebaseapp.com",
-        databaseURL: "https://reacttodoapp-c643b.firebaseio.com",
-        projectId: "reacttodoapp-c643b",
-        storageBucket: "reacttodoapp-c643b.appspot.com",
-        messagingSenderId: "673840687356"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
     firebase.initializeApp(config);
     
 } catch (e) {
-    
+    console.log(e);
 }
 
 export var firebaseRef = firebase.database().ref();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 var webpack = require('webpack');
+var path  = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development"
+var envFile = require('node-env-file');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+	envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+	console.log(e);
+}
 
 module.exports = {
   	entry: [
@@ -15,12 +24,23 @@ module.exports = {
       new webpack.ProvidePlugin({
         '$': 'jquery',
         'jQuery': 'jquery'
-      })
-			// new webpack.optimize.UglifyJsPlugin({
-			// 	compressor: {
-			// 		warning: false
-			// 	}
-			// })
+      }),
+			new webpack.optimize.UglifyJsPlugin({
+				compress: {
+					warnings: false
+				}
+			}),
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+					API_KEY: JSON.stringify(process.env.API_KEY),
+					AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+					DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+					PROJECT_ID: JSON.stringify(process.env.PROJECT_ID),
+					STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+					MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
+				}
+			})
     ],
   	output: {
     	path: __dirname,
